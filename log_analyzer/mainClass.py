ce050af1fb5afc3dd9643f48d51d0289c2583c7e
AkshayD110 <akshayd110@gmail.com>
2018-02-19 00:16:20 +0530

    compacting and fine tuning the class

--- log_analyzer/mainClass.py
+++ log_analyzer/mainClass.py
@@ -1,6 +1,8 @@
-
+from log_analyzer import analyzer
 def main():
-    pass
+    path=r'C:\Users\akshdesh.ORADEV\Documents\books\python\work2\Problamatictime_Logs.zip'
+    analysis_obj=analyzer.analyzer(path)
+    analysis_obj.unzip_files()
 
 if __name__ == '__main__':
     main()
