68a2888a18ae30e64c7e3e7f219f0be6f95be026
AkshayD110 <akshayd110@gmail.com>
2018-02-19 23:28:23 +0530

    compacting and fine tuning the class

--- log_analyzer/analyzer.py
+++ log_analyzer/analyzer.py
@@ -1,14 +1,16 @@
 import os
 import zipfile
+import re
 
 class analyzer(object):
 
     'This class analysis the logs'
-    def __init__(self, zippath):
-        self._zippath=zippath
+    def __init__(self, zippath, service):
+        self.zippath=zippath
+        self.service=service
 
     def __repr__(self):
-        return f'{self.__class__.__name__} Class analyzing logs from {zippath}'
+        return f'{self.__class__.__name__} Class analyzing logs from {self._zippath}, for the service - {self._service}'
 
     @property
     def zippath(self):
@@ -16,15 +18,31 @@ class analyzer(object):
 
     @zippath.setter
     def zippath(self, zippath):
-        self._zippath=zippath
+        self._zippath = zippath
+
+    @property
+    def service(self):
+        return self._service
+
+    @service.setter
+    def service(self,service):
+        self._service = service  # need to check if the service looking for exists in json later here
 
     def unzip_files(self):
-        with zipfile.ZipFile(self._zippath, 'r') as zip_ref:
-            logs_extractpath=os.path.split(self._zippath)
+        with zipfile.ZipFile(self.zippath, 'r') as zip_ref:
+            logs_extractpath=os.path.split(self.zippath)
             zip_ref.extractall(logs_extractpath[0])
 
 
     def check_for_logs(self):
+        logs_path=os.path.split(self.zippath)
+        word = re.split(r'\.(?!\d)', logs_path[1])
+        #logs_path=logs_path[0].join(word[0])
+        logs_path=os.path.join(logs_path[0], word[0])
+        print(logs_path)
+
+    def time_parser(self):
         pass
+
 
 
