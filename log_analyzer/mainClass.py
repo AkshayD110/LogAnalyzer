c35ca9897653eb75df46fc65eee17a1f403b5337
AkshayD110 <akshayd110@gmail.com>
2018-02-25 00:09:15 +0530

    case to handle if file is not present added

--- log_analyzer/mainClass.py
+++ log_analyzer/mainClass.py
@@ -1,7 +1,7 @@
 from log_analyzer import analyzer
 def main():
     path=r'C:\Users\akshdesh.ORADEV\Documents\books\python\work2\Problamatictime_Logs.zip'
-    service="BICS"
+    service="BICS" #This has to be a user input later
     analysis_obj=analyzer.analyzer(path,service)
     analysis_obj.unzip_files()
     analysis_obj.check_for_logs()
