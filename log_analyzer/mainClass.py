f6dbc8f9d9b0c5764007377f83cb7fea018c7ba8
AkshayD110 <akshayd110@gmail.com>
2018-02-25 22:39:57 +0530

    converting input to uppercase

--- log_analyzer/mainClass.py
+++ log_analyzer/mainClass.py
@@ -1,7 +1,11 @@
 from log_analyzer import analyzer
+
+
 def main():
-    path=r'C:\Users\akshdesh.ORADEV\Documents\books\python\work2\Problamatictime_Logs.zip'
+    path=r'C:\Users\akshdesh.ORADEV\Documents\books\python\work2\Problamatictime_Logs.zip' #This will be user input
+    # later
     service="BICS" #This has to be a user input later
+    service=service.upper()
     analysis_obj=analyzer.analyzer(path,service)
     analysis_obj.unzip_files()
     analysis_obj.check_for_logs()
